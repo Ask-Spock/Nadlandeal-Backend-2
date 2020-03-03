dc3ec21828a98bd12dcc436d93c45c2078779e61
talintrip <talintrip@gmail.com>
2020-03-03 09:59:24 +0200

    first commit

---
+++ scripts/main-script.js
@@ -0,0 +1,17 @@
+
+
+  /*Right Menu JavaScript*/
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+      document.getElementById("responsive-menu").style.display = "none";
+      document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0";
+      document.body.style.backgroundColor = "white";
+      document.getElementById("responsive-menu").style.display = "initial";
+
+    }
